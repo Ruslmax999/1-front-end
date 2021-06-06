6046b2210e0a8f7e69a1c8472ad841703b6fa560
Ruslmax999 <ruslmax99@gmail.com>
2021-06-06 13:16:37 +0300

    practic

---
+++ students/muginov_ruslan/lesson_practic/src/js/script-debug.js
@@ -0,0 +1,5 @@
+const fullname = "Ruslan Muginov";
+const mycatname = "opel";
+const sityliave = "Poltava"
+const message = " My name is " + fullname + "." + "My cat name" + mycatname + "."
+console.log(message)
